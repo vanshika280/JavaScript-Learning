012769d80a8f5187023c02ef7709a68732ee3fbe
vanshika280 <158443698+vanshika280@users.noreply.github.com>
2025-08-06 05:36:55 +0000

    learning about variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,8 @@
+const accountID=12324
+let accountEmail= "vanshikasardana7@gmail.com"
+var accountPassword ="32311"
+accountCity = "Jaipur"
+//accountID=2
+let accounState;
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountCity,accounState])
