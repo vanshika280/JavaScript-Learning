f59fca4e466df63cb309da897b013bf2ab236bad
vanshika280 <158443698+vanshika280@users.noreply.github.com>
2025-08-06 06:40:46 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) // we are using node.js not browser
+console.log(3
+    +3)//dont write code like this , it decreases code readibility
+    let name="Vanshika"
+    let age=19
+    let isLoggedIn =true
+    let state = null;
+    let a;
+
+
+    /*
+    Primitive data types
+    // number => 2 to power 53
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+
+    Non primitive datatypes
+    // object
+
+    */ 
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null)
+console.log(typeof undefined)
