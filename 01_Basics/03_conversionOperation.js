bd419fad8738e711ed4835b1fee5c15bf11a1973
vanshika280 <158443698+vanshika280@users.noreply.github.com>
2025-08-07 16:52:50 +0000

    conversion of dataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score ="33";
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let score2="33abc";
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2)
+console.log( valueInNumber2)
+let a=undefined;
+let valueInNumber3 = Number(a);
+console.log(typeof valueInNumber3)
+console.log( valueInNumber3)
+//NaN (not a number)
+//let b=undefined
+// true value is convered in 1 and false value is converted in 0 and viceversa
+let c=0;
+let result= Boolean(c)
+console.log(result);
